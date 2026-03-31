783f5c548c148a2a7da0dde74aa21bd9abbe5853
Satyam Yadav <>
2026-03-31 10:44:21 +0530

    Assignment 1 HTML + JS

---
+++ assignment1.js
@@ -0,0 +1,32 @@
+var num = 10;
+console.log(num);
+console.log(typeof(num));
+num = false;
+console.log(num);
+console.log(typeof(num));
+num = "SATYAM YADAV";
+console.log(num);
+console.log(typeof(num));
+num = null;
+console.log(num);
+num = undefined;
+console.log(num);
+console.log(typeof(num));
+
+//FUNCTION
+var sayHello = function ()
+{
+    alert('hello world !');
+}
+
+//ARRAY 
+var numberArray = [1,2,3];
+var animals = new Array ("cat","dog","mouse","lion");
+
+//Object
+var person = 
+{
+name : 'BARACK HUSSEIN OBAMA II',
+age : '51',
+title : '44th President of the United States'
+}
